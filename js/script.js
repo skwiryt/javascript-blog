345ebc9769963655c085a2a3d6fa7c14c9688e05
skwiryt <mzbprobny@gmail.com>
2021-01-13 16:18:28 +0100

    Add generation of links

--- js/script.js
+++ js/script.js
@@ -41,9 +41,41 @@
         pickedPost.classList.add("active");
         console.log(pickedPost);
     }
-    
-    const links = document.querySelectorAll(`${optTitleListSelector} a`);    
-    for(let link of links){
-        link.addEventListener('click', titleClickHandler);
+       
+    //generate links with article titles
+
+    function generateTitleLinks(){        
+        
+        /* remove contents of titleList */
+        const titleList = document.querySelector(optTitleListSelector);
+        titleList.innerHTML = "";
+
+        /* for each article */
+        const articles = document.querySelectorAll(optArticleSelector);
+        for (const article of articles){
+          /* get the article id */
+            const articleId = article.id; 
+
+          /* find the title element */
+            const titleElement = article.querySelector(optTitleSelector);   
+          /* get the title from the title element */
+            const articleTitle = titleElement.textContent;
+      
+          /* create HTML of the link */
+            const liElement = document.createElement("li");
+            liElement.innerHTML = `<a href="#${articleId}"><span>${articleTitle}</span></a>`;
+      
+          /* insert link into titleList */
+            titleList.append(liElement);
+        }
+
+        //adding listeners to generated links
+        const links = document.querySelectorAll(`${optTitleListSelector} a`);    
+        for(let link of links){
+            link.addEventListener('click', titleClickHandler);
+        }
+        
     }
+      
+    generateTitleLinks();    
 }
