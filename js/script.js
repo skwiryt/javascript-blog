84d6f96cda3539682a71d356e345bfcb8ae34cb1
skwiryt <mzbprobny@gmail.com>
2021-01-14 18:55:37 +0100

    Add two ways to address eslint restrictions

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 {
+  /**/
+  /**/
+  // Two important ways to address style problems with airbnb are shown in this file.
+  // Look for comments like this.
+  /**/
+  /**/
   // options
   const optArticleSelector = '.post';
   const optTitleSelector = '.post-title';
   const optTitleListSelector = '.titles';
-  const titleClickHandler = (event) => {
+  /**/
+  /**/
+  // First dealing with eslint. Use additional name of function in function expression.
+  /**/
+  /**/
+  const titleClickHandler = function titleClickHandler(event) {
     event.preventDefault();
-    const clickedElement = event.target.parentElement;
+    const clickedElement = this;
 
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll(`${optTitleListSelector} a.active`);
@@ -17,7 +28,6 @@
     clickedElement.classList.add('active');
 
     /* [DONE] remove class 'active' from all articles */
-
     const activeArticles = document.querySelectorAll(`${optArticleSelector}.active`);
     activeArticles.forEach((activeArticle) => {
       activeArticle.classList.remove('active');
@@ -40,7 +50,13 @@
 
     /* for each article */
     const articles = document.querySelectorAll(optArticleSelector);
-    articles.forEach((article) => {
+    /**/
+    /**/
+    // Second method is simply to tell eslint to take a breake
+    /**/
+    /**/
+    /* eslint-disable-next-line no-restricted-syntax */
+    for (const article of articles) {
       /* get the article id */
       const articleId = article.id;
 
@@ -55,7 +71,7 @@
 
       /* insert link into titleList */
       titleList.append(liElement);
-    });
+    }
 
     // adding listeners to generated links
     const links = document.querySelectorAll(`${optTitleListSelector} a`);
